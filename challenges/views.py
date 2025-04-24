b873a2e9680f95ce5e4cb9e2e56c4fa6e5e64c66
Le Di Dan <danld@rikkeisoft.com>
2025-04-24 17:06:11 +0700

    learn template

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse("Challenges home page")
+
+def monthly_challenge(request, month):
+    months = {
+        "january": "1",
+        "february": "2",
+        "march": "3",
+        "april": "4",
+        "may": "5",
+        "june": "6",
+        "july": "7",
+        "august": "8",
+        "september": "9",
+        "october": "10",
+        "november": "11",
+        "december": "12"
+    }
+    
+    challenges = [
+        "Learn Django",
+        "Learn Japanese"
+    ]
+    print(month)
+    return render(request, "challenges/list.html", {
+        "challenges": challenges,
+        "month": month
+    })
+
